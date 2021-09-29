062bc38c3335443220d2098a875aa8488da339c6
AlexandrDulovec <79139997+AlexandrDulovec@users.noreply.github.com>
2021-09-29 14:25:49 +0200

    ...

---
+++ bash.sh
@@ -0,0 +1,43 @@
+mv martin_kokes.txt/ucitele
+cd zaci
+mv ossp/ucitele/filip_dubcek.txt ossp/zaci/filip_dubcek.txt
+tree
+mv ossp/ucitele/alexandr_dulovec.txt ossp/zaci/alexandr_dulovec.txt
+tree
+mv ossp/ucitele/ondrej_cabelka.txt ossp/zaci/ondrej_cabelka.txt
+tree
+mv ossp/ucitele/zuzana_sahulkova.txt ossp/zaci/zuzana_sahulkova.txt
+tree
+mv ossp/zaci/martin_kokes.txt ossp/ucitele/martin_kokes.txt
+tree
+mv ossp/zaci/lenka_sklenarova.txt ossp/ucitele/lenka_sklenarova.txt
+tree
+cd ossp
+cd zaci
+mkdir 2.C
+mkdir 3.C
+tree
+cd
+tree
+mv ossp/zaci/alexandr_dulovec.txt ossp/zaci/2.C/alexandr_dulovec.txt
+tree
+mv ossp/zaci/daniel_sumban.txt ossp/zaci/2.C/daniel_sumban.txt
+tree
+mv ossp/zaci/filip_dubcek.txt ossp/zaci/2.C/filip_dubcek.txt
+tree
+mv ossp/zaci/jan_nicolas_sefzig.txt ossp/zaci/2.C/jan_nicolas_sefzig.txt
+tree
+mv ossp/zaci/jaroslav_kasak.txt ossp/zaci/2.C/jaroslav_kasak.txt
+tree
+mv ossp/zaci/jaroslav_melichar.txt ossp/zaci/2.C/jaroslav_melichar.txt
+tree
+mv ossp/zaci/martin_hruska.txt ossp/zaci/2.C/martin_hruska.txt
+mv ossp/zaci/matej_posik.txt ossp/zaci/2.C/matej_posik.txt
+tree
+mv ossp/zaci/ondrej_cabelka.txt ossp/zaci/2.C/ondrej_cabelka.txt
+mv ossp/zaci/zuzana_sahulkova.txt ossp/zaci/2.C/zuzana_sahulkova.txt
+tree
+mv ossp/zaci/karel_nakladal.txt ossp/zaci/3.C/karel_nakladal.txt
+tree
+history
+alexandr1312@DESKTOP-NHB4INH:~$
